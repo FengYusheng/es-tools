4636c3e93478aa9b2aeb8ca55b981b1368bb3b28
fengyusheng <maverick.fengys@gmail.com>
2019-03-09 13:18:32 +0800

    add SERVER and PORT options

--- runAnalyzer.py
+++ runAnalyzer.py
@@ -7,10 +7,18 @@ def optParser():
         "prog" : "runAnalyzer",
         "description" : '''Call ES Analyzer API''',
         "formatter_class" : argparse.RawDescriptionHelpFormatter,
-        "argument_default" : ""
+        "argument_default" : argparse.SUPPRESS,
+        "conflict_handler" : "error"
     }
 
-
+    parser = argparse.ArgumentParser(**kwargs)
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
+    parser.add_argument('-s', '--server', action='store', type=str, required=False, default='localhost', help='the address of your elasticesearch server.', dest='server', metavar='Server')
+    parser.add_argument('-p', '--port', action='store', type=str, required=False, default='9220', help='the port number which your elasticsearch is listening on', dest='port', metavar='Port')
+    
+    options = parser.parse_args()
+    return vars(options)
 
 if __name__ == '__main__':
-    optParser()
+    options = optParser()
+    print(options)
