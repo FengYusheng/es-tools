d31ab3f522bb1132148dc2646cd685797cf35c4d
fengyusheng <maverick.fengys@gmail.com>
2019-03-24 11:56:51 +0800

    pyhton call java method

--- javaArabicAnalyzer.py
+++ javaArabicAnalyzer.py
@@ -1,23 +1,12 @@
 # -*- coding: utf-8 -*-
 # https://github.com/kivy/pyjnius
 # https://jpype.readthedocs.io/en/latest/userguide.html
-# java -cp lucene-arabic-analyzer-1.1.2.jar:arabic-analyzer.jar arabic.analyzer.Library "الرَّحْمَنِ"
-#
-# CLASSPATH = os.path.realpath(os.path.expanduser(os.path.expandvars(os.path.abspath('lucene-arabic-analyzer-1.1.2.jar'))))
-#
-# from jpype import *
-#
-# startJVM(getDefaultJVMPath(), "-ea", "-Djava.class.path=%s" % CLASSPATH)
-# # https://hustleplay.wordpress.com/2010/02/18/jpype-tutorial/
-# msarhan = JPackage('com').github.msarhan.lucene
-# ArabicRootExtractorStemmer = JClass("com.github.msarhan.lucene.ArabicRootExtractorStemmer")
-# stemmer = ArabicRootExtractorStemmer()
-# jString = java.lang.String("الرَّحْمَنِ")
-# stemmer.stem(jString)
-# shutdownJVM()
+# java -cp lucene-arabic-analyzer-1.1.2.jar:arabic-analyzer.jar arabic.analyzer.Library
+
+
 import subprocess
 completed_process = subprocess.run(
-    'java -cp lucene-arabic-analyzer-1.1.2.jar:arabic-analyzer.jar arabic.analyzer.Library "الرَّحْم"',
+    ' java -cp ./jars/*:arabic-analyzer.jar arabic.analyzer.Library رحم الرَّحْمَنِ ',
     stdout=subprocess.PIPE,
     stderr=subprocess.PIPE,
     shell=True,
@@ -25,4 +14,4 @@ completed_process = subprocess.run(
     universal_newlines=True
 )
 response = completed_process.stdout
-print(response)
+print(repr(response))
