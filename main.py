fadac4490e2eba0c2cab0d30738b05847c3ca848
skywalker <maverick.fengys@gmail.com>
2019-06-15 07:59:14 +0800

    new mapping

--- main.py
+++ main.py
@@ -72,15 +72,15 @@ def numerals():
 
 
 def derivation():
-    # process_inflection_in_a_csv_file('./data/derivation.csv', analyzer='rbl_ara')
-    # process_inflection_in_a_csv_file('./data/derivation.csv', analyzer='rbl_ara_folding')
+    process_inflection_in_a_csv_file('./data/derivation.csv', analyzer='rbl_ara')
+    process_inflection_in_a_csv_file('./data/derivation.csv', analyzer='rbl_ara_folding')
     # process_inflection_in_a_csv_file('./data/derivation.csv', analyzer='ar_std_lem_folding1')
     # process_inflection_in_a_csv_file('./data/derivation.csv', analyzer='ar_std_lem_folding2')
     process_inflection_in_a_csv_file('./data/derivation.csv', analyzer='ar_std_lem')
     # process_inflection_in_a_csv_file('./data/derivation.csv', analyzer='msarhan')
-    records = collect_analyzer_results('./data/derivation_ar_std_lem_report.csv', 'ar_std_lem')
-    # records = add_new_analyzer_results(records, './data/derivation_rbl_ara_folding_report.csv', 'rbl_ara_folding')
-    # records = add_new_analyzer_results(records, './data/derivation_ar_std_lem_folding1_report.csv', 'ar_std_lem_folding1')
+    records = collect_analyzer_results('./data/derivation_rbl_ara_report.csv', 'rbl_ara')
+    records = add_new_analyzer_results(records, './data/derivation_rbl_ara_folding_report.csv', 'rbl_ara_folding')
+    records = add_new_analyzer_results(records, './data/derivation_ar_std_lem_report.csv', 'ar_std_lem')
     # records = add_new_analyzer_results(records, './data/derivation_ar_std_lem_folding2_report.csv', 'ar_std_lem_folding2')
     # records = add_new_analyzer_results(records, './data/derivation_msarhan_report.csv', 'msarhan')
     # records = add_new_analyzer_results(records, './data/derivation_ara_hunspell_report.csv', 'ara_hunspell')
@@ -89,15 +89,13 @@ def derivation():
 
 
 def derivation_for_inaccuracy():
-    # process_inflection_in_a_csv_file('./data/derivation2.csv', analyzer='rbl_ara')
-    # process_inflection_in_a_csv_file('./data/derivation2.csv', analyzer='rbl_ara_folding')
-    # process_inflection_in_a_csv_file('./data/derivation2.csv', analyzer='ar_std_lem_folding1')
-    # process_inflection_in_a_csv_file('./data/derivation2.csv', analyzer='ar_std_lem_folding2')
     process_inflection_in_a_csv_file('./data/derivation2.csv', analyzer='ar_std_lem')
+    process_inflection_in_a_csv_file('./data/derivation2.csv', analyzer='rbl_ara')
+    process_inflection_in_a_csv_file('./data/derivation2.csv', analyzer='rbl_ara_folding')
     # process_inflection_in_a_csv_file('./data/derivation2.csv', analyzer='msarhan')
     records = collect_analyzer_results('./data/derivation2_ar_std_lem_report.csv', 'ar_std_lem')
-    # records = add_new_analyzer_results(records, './data/derivation2_rbl_ara_folding_report.csv', 'rbl_ara_folding')
-    # records = add_new_analyzer_results(records, './data/derivation2_ar_std_lem_folding1_report.csv', 'ar_std_lem_folding1')
+    records = add_new_analyzer_results(records, './data/derivation2_rbl_ara_report.csv', 'rbl_ara')
+    records = add_new_analyzer_results(records, './data/derivation2_rbl_ara_folding_report.csv', 'rbl_ara_folding')
     # records = add_new_analyzer_results(records, './data/derivation2_ar_std_lem_folding2_report.csv', 'ar_std_lem_folding2')
     # records = add_new_analyzer_results(records, './data/derivation2_msarhan_report.csv', 'msarhan')
     # records = add_new_analyzer_results(records, './data/derivation2_ara_hunspell_report.csv', 'ara_hunspell')
@@ -230,11 +228,13 @@ def run():
     verbs_present_tense()
     nouns_combined_wiz_possessive_pronoun()
     nouns_with_pronouns()
-    # nouns_singular_form()
-    # nouns_pluarl_form()
-    # nouns_with_article()
+    nouns_singular_form()
+    nouns_pluarl_form()
+    nouns_with_article()
     nouns_singular_plual_and_article()
     preposistion()
+    derivation()
+    derivation_for_inaccuracy()
 
 
 
